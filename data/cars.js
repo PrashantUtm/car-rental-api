58fe689025032d26cf6d705474ae25ddfb8bce14
Prashant Purmah <prashant.purmah@sdworx.com>
2024-07-13 01:08:17 +0400

    update data

--- data/cars.js
+++ data/cars.js
@@ -6,7 +6,7 @@ const cars = [
         capacity: 5,
         dailyRate: 1000,
         fuel: 'unleaded',
-        ownerId: '1',
+        ownerId: '66918bf5c4fd29de0848c0dd',
         transmission: 'manual',
         type: "Sedan",
         photo: 'https://toyotamauritius.com/sites/default/files/models/colors/5.png'
@@ -18,7 +18,7 @@ const cars = [
         capacity: 4,
         dailyRate: 800,
         fuel: 'diesel',
-        ownerId: '2',
+        ownerId: '66918bf5181638291b20d171',
         transmission: 'automatic',
         type: "Hatchback",
         photo: 'https://cars.suzuki.co.uk/_next/image/?url=https%3A%2F%2Fmedia.umbraco.io%2Fsuzuki-gb%2Fhf4b0n10%2Fswift-module-50-alpha-motion-mobile-2x.png&w=828&q=75'
@@ -30,7 +30,7 @@ const cars = [
         capacity: 12,
         dailyRate: 1500,
         fuel: 'unleaded',
-        ownerId: '3',
+        ownerId: '66918bf5181638291b20d171',
         transmission: 'manual',
         type: "Van",
         photo: 'https://toyotamauritius.com/sites/default/files/models/colors/hiace-colour_0.jpg'
